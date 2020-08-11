42984fa995502d4248e199f8d3197638be4144f2
lc <1033581609@qq.com>
2020-08-11 16:09:36 +0800

    讲师详情弹出框

--- src/views/lecturer/index.js
+++ src/views/lecturer/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Frame from "../../common/component/frame";
 import "../../common/css/teacher.css";
 import { connect } from "react-redux";
@@ -6,10 +6,20 @@ import getLecturers from "../../store/action/getLecturers";
 import LecturerTab from "./tab";
 import Join from "./join";
 import Footer from "../../common/component/footer";
+import LecturerAlert from "./lecturerAlert";
 
 const Lecturer = props => {
+    const [show, setShow] = useState(false);
+    const [alertDate, setAlertData] = useState(null);
     const { data, dispatch } = props;
     const newData = [];
+    function showAlert(data) {
+        setAlertData(data);
+        setShow(true);
+    }
+    function hideAlert() {
+        setShow(false);
+    }
     for (let i = 0; i < data.length; i += 3) {
         const newArr = [];
         let end = i + 3;
@@ -26,16 +36,19 @@ const Lecturer = props => {
         dispatch(getLecturers());
     }, []);
     return (
-        <Frame>
-            <div className="teacher_banner">
-                <h2>
-                    <span>妙味团队</span>
-                </h2>
-                <LecturerTab data={data} newData={newData} />
-            </div>
-            <Join />
-            <Footer />
-        </Frame>
+        <div>
+            <Frame>
+                <div className="teacher_banner">
+                    <h2>
+                        <span>妙味团队</span>
+                    </h2>
+                    <LecturerTab data={data} newData={newData} showAlert={showAlert} />
+                </div>
+                <Join />
+                <Footer />
+            </Frame>
+            {show ? <LecturerAlert data={alertDate} hideAlert={hideAlert} /> : ""}
+        </div>
     );
 };
 
--- src/views/lecturer/tab.js
+++ src/views/lecturer/tab.js
@@ -1,8 +1,7 @@
 import React from "react";
 import Tab from "../../common/component/tabs";
-
-const LecturerTab = props => {
-    const { data, newData } = props;
+export default function LecturerTab(props) {
+    const { data, newData, showAlert } = props;
     return data.length < 1 ? (
         ""
     ) : (
@@ -12,8 +11,30 @@ const LecturerTab = props => {
                 return (
                     <ul className="lecturer_list">
                         {data.map(item => {
+                            let point = {};
                             return (
-                                <li key={item.id}>
+                                <li
+                                    key={item.id}
+                                    onTouchStart={e => {
+                                        let touch = e.changedTouches[0];
+                                        point.x = touch.pageX;
+                                        point.y = touch.pageY;
+                                    }}
+                                    onTouchEnd={e => {
+                                        let touch = e.changedTouches[0];
+                                        let nowPoint = {
+                                            x: touch.pageX,
+                                            y: touch.pageY,
+                                        };
+                                        //console.log(point,nowPoint);
+                                        if (
+                                            Math.abs(nowPoint.x - point.x) < 5 &&
+                                            Math.abs(nowPoint.y - point.y) < 5
+                                        ) {
+                                            showAlert(item);
+                                        }
+                                    }}
+                                >
                                     <img src={item.icon} />
                                     <p>{item.title}</p>
                                 </li>
@@ -22,8 +43,6 @@ const LecturerTab = props => {
                     </ul>
                 );
             }}
-        ></Tab>
+        />
     );
-};
-
-export default LecturerTab;
+}
