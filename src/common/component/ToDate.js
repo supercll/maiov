077cfc281d80f864c4d17b2488249e75fff91b10
lc <1033581609@qq.com>
2020-08-12 10:06:39 +0800

    引入dayjs格式化时间

--- src/common/component/ToDate.js
+++ src/common/component/ToDate.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import day from "dayjs";
 /*
     1分钟内 :  "刚刚", // 60000
     1小时内 :  "n分钟之前", // 3600000
@@ -6,21 +7,21 @@ import React from 'react';
     1周内 :  "n天之前",//  86400000*7 =  
     time
 */
-function ToDate(props){
-    let {time} = props;
+function ToDate(props) {
+    let { time } = props;
     let nowTime = Date.now();
     let newTime = new Date(time).getTime();
     let disTime = nowTime - newTime;
     //console.log(newTime);
-    if(disTime < 60000){
+    if (disTime < 60000) {
         return "刚刚";
-    } else if(disTime < 3600000){
-        return parseInt(disTime/60000) + "分钟之前";
-    } else if(disTime < 86400000){
-        return parseInt(disTime/3600000) + "小时之前";
-    } else if(disTime < 604800000){
-        return parseInt(disTime/86400000) + "天之前";
+    } else if (disTime < 3600000) {
+        return parseInt(disTime / 60000) + "分钟之前";
+    } else if (disTime < 86400000) {
+        return parseInt(disTime / 3600000) + "小时之前";
+    } else if (disTime < 604800000) {
+        return parseInt(disTime / 86400000) + "天之前";
     }
-    return time;
+    return day(time).format("YYYY年M月D日H时");
 }
 export default ToDate;
